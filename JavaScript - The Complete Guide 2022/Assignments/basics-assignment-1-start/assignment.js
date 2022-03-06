444ba302cb9cfa427559f4eadcf786cce92aafca
avi-somech <abraham.somech@mail.mcgill.ca>
2022-03-06 13:11:59 -0500

    Setting up different sections

---
+++ JavaScript - The Complete Guide 2022/Assignments/basics-assignment-1-start/assignment.js
@@ -0,0 +1,14 @@
+let fictionalInput = 12;
+let result;
+
+result = 18 + fictionalInput;
+
+result = result - 10;
+result = result * 2;
+result = result / 10;
+
+alert(result);
+alert(fictionalInput);
+
+
+
