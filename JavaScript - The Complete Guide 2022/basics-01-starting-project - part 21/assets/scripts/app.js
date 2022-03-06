42a3fd978402883ea26af393d367d37fcdaac18a
avi-somech <abraham.somech@mail.mcgill.ca>
2022-03-06 12:49:15 -0500

    Initial commit - section 2 - part 21

---
+++ JavaScript - The Complete Guide 2022/basics-01-starting-project - part 21/assets/scripts/app.js
@@ -0,0 +1,13 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+currentResult = add(1,3);
+
+addBtn.addEventListener('click', add);
+
+outputResult(currentResult, calculationDescription);
