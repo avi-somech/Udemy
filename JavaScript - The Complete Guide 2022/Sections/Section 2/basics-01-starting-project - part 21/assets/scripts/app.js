2896760bbff4ea4c0a4fb1f3dc8763ce00e819fc
avi-somech <abraham.somech@mail.mcgill.ca>
2022-03-07 12:34:34 -0500

    JS section 2 - 38,39

--- JavaScript - The Complete Guide 2022/Sections/Section 2/basics-01-starting-project - part 21/assets/scripts/app.js
+++ JavaScript - The Complete Guide 2022/Sections/Section 2/basics-01-starting-project - part 21/assets/scripts/app.js
@@ -1,16 +1,51 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
 //he said that in general a function should not mix stuff from outside. 
 //in this case, its fine because its global but its only global. 
 //it is not a mix of global and local stuff
 //he said we will get a feel for when it is fine and when not
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
