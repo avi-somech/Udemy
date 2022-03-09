6eadbdb1e80da9885ac7e14bb83af4670971971e
avi-somech <abraham.somech@mail.mcgill.ca>
2022-03-09 08:59:53 -0500

    js: did assignment 2 section 2

---
+++ JavaScript - The Complete Guide 2022/Assignments/basics-assignment-2-start/assignment.js
@@ -0,0 +1,31 @@
+const task3Element = document.getElementById('task-3');
+
+
+function alertText(){
+    alert('hi this is an alert');
+}
+
+function alertParam(param){
+    alert(param);
+}
+
+alertText();
+alertParam("Hiiii");
+
+task3Element.addEventListener('click', alertText)
+
+//this is how I wrote the function but note it will add numbers and concatnenate strings...
+//but if you pass in numbers as strings it will work
+// function concatenate3(one, two, three) {
+//     return one+two+three;
+// }
+
+//this is how he did it: will make numbers strings as well
+//So even if someone passes in the numbers as numbers, this code will treat them as strings
+function concatenate3(str1, str2, str3){
+    const combined = `${str1} ${str2} ${str3}`;
+    return combined;
+}
+
+alert(concatenate3('a','b','c'));
+alert(concatenate3(1,2,3));
